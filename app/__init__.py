b2382449b6025436ce71334f0d8f987a19bdaf52
amala <amalakkad93@outlook.com>
2023-09-24 16:34:32 -0700

    get restaurant detail is done

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.home_restaurants import home_restaurants
+from .api.restaurants_routes import home_restaurants
 from .seeds import seed_commands
 from .config import Config
 
--- app/api/restaurants_routes.py
+++ app/api/restaurants_routes.py
@@ -10,7 +10,7 @@ home_restaurants = Blueprint('restaurants', __name__)
 
 @home_restaurants.route("/")
 def get_popular_restaurants():
-
+  """returns a all restaurant order by popularity"""
   restaurants = db.session.query(Restaurant).join(Review, Restaurant.id == Review.restaurant_id).\
     group_by(Restaurant.id).\
     order_by(func.avg(Review.stars).desc()).\
@@ -19,3 +19,9 @@ def get_popular_restaurants():
   all_restaurants = {'restaurants': [restaurant.to_dict() for restaurant in restaurants]}
   print('restaurants**', all_restaurants)
   return all_restaurants
+
+@home_restaurants.route("/<int:id>")
+def get_restaurant_by_id(id):
+    """returns a single restaurant by the given id provided as a route parameter"""
+    one_restaurant = Restaurant.query.get(id).to_dict()
+    return one_restaurant
