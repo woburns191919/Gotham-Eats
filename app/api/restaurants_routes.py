9530c98c0718fcf9f801c6694e1c66169ddb9994
will <woburns191919>
2023-09-24 19:26:30 -0500

    form created

--- app/api/restaurants_routes.py
+++ app/api/restaurants_routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import app
 from flask_login import login_required
 from app.models import User, Restaurant, Review, db
 from sqlalchemy import func, distinct, or_, desc
+from ..forms import RestaurantForm
 # from ..models.restaurants import
 
 home_restaurants = Blueprint('restaurants', __name__)
@@ -25,3 +26,27 @@ def get_restaurant_by_id(id):
     """returns a single restaurant by the given id provided as a route parameter"""
     one_restaurant = Restaurant.query.get(id).to_dict()
     return one_restaurant
+
+@home_restaurants.route("/new", methods=["POST"])
+def create_new_restaurant():
+  """creates a new restaurant"""
+  form = RestaurantForm()
+
+  data = request.get_json()
+  if form.validate_on_submit():
+    new_restaurant = Restaurant(
+      owner_id = data["owner_id"],
+      streetAddress = data["street_address"],
+      city = data["city"],
+      state = data["state"],
+      postalCode = data["postal_code"],
+      country = data["country"],
+      hours = data["hours"],
+      previmg = data["previmg"]
+    )
+
+    print(new_restaurant)
+    db.session.add(new_restaurant)
+    db.session.commit()
+    return jsonify(message = "Successfully created new restaurant"), 201
+  return jsonify(errors=form.errors), 400
