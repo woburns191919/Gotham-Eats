c3b675c0226debbe8826c3a099a1da98732793d1
amala <amalakkad93@outlook.com>
2023-09-30 16:34:40 -0700

    fix import issue

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,4 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .restaurant_form import RestaurantForm
+from .menu_item_form import MenuItemForm
 # from .image_form import ImageForm
